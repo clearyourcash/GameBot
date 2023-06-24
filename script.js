73b2e73fda19b93b6af5d04abef0a7ccf51f3c31
Evgeniya Bystrikova <byseum@mail.ru>
2023-06-25 00:26:40 +0300

    создан бот - Угадай число от 1 до 100 (исправлен оператор условия на if)

--- script.js
+++ script.js
@@ -4,20 +4,23 @@ const makeAGuess = function(n){
 n = 87;
 let randomNumber = prompt("Угадай число от 1 до 100. Введите число");
 {
-  if (randomNumber === null){
-    alert("Игра окончена!")
+if (randomNumber === null){
+alert("Игра окончена!")
+return
 }
-  else if (randomNumber > n){
-    alert("Загаданное число меньше")
-  } 
-    else if (randomNumber < n){
-      alert("Загаданное число больше.")
-  }
-    else if (isNaN(randomNumber)){
-      alert("Введите число!")
-  } 
-  else  alert ("Поздравляю, Вы угадали!!!")
+if (isNaN(randomNumber)){
+alert("Введите число!")
 }
-  return makeAGuess();
+if (randomNumber == n) {
+alert("Поздравляю, Вы угадали!!!")
+}
+if (randomNumber > n){
+alert("Загаданное число меньше")
+}
+if (randomNumber < n){
+alert("Загаданное число больше")
+}
+}
+return makeAGuess();
 }
 makeAGuess();
